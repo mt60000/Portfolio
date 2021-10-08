6e61d4145c077a9a7416aa8b13732d46edbb97b0
mt60000 <niruyaku45@gmail.com>
2021-10-08 08:54:39 +0000

    routes記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update] do
+    resources :diaries
+  end
+
+  patch "/users/:id/leave" => "users#leave"
+
+  scope "/diaries" do
+    post "/comments" => "comments#create"
+    delete "/comments" => "comments#destroy"
+    post "/stamps" => "stamps#create"
+    delete "/stamps" => "stamps#destroy"
+  end
+
+  resources :groups do
+    get "/join" => "groups#join"
+    get "/leave" => "groups#leave"
+  end
 end
