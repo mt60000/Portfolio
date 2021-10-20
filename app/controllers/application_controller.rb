11add02f5006fe066d34ff7a3b781ba6af92f8cb
mt60000 <niruyaku45@gmail.com>
2021-10-20 11:46:39 +0000

    サインイン機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_up_path_for(resource)
     root_path
@@ -12,4 +13,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     top_path
   end
+
+
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
